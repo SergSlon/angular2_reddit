f2afba0ac59cb0c2da329185475a734abe296772
SergSlon <liamin.web@gmail.com>
2016-11-01 15:35:06 +0200

    Adding Interaction - created a function named addArticle that defines what we want to do when the button is clicked - added a (click) attribute on the button that says “call the function addArticle when this button is pressed” - added the attribute #newtitle and #newlink to the <input> tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 }
